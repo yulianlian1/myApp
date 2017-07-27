47be6e0be002949b24b3777d175ab9f4d411ed76
Unknown <yulianlian_host@126.com>
2017-07-27 12:08:11 +0800

    Revert "sss"
    This reverts commit 0782432d4ea3f37498b1c38ef5c472fc54e9b727.

--- app.js
+++ app.js
@@ -11,5 +11,9 @@ http.createServer(function (request, response) {
 	response.end('Hello World\n');
 }).listen(8888);
 
+<<<<<<< HEAD
 // 终端打印如下信息--修改测试-修改测试
+=======
+// 终端打印如下信息
+>>>>>>> parent of 0782432... sss
 console.log('Server running at http://127.0.0.1:8888/');
